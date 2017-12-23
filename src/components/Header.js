b45f647260c16432260b94742251b8ba72c03aec
alik <anothermalik@gmail.com>
2017-12-23 10:50:05 +0700

    add container

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 const Header = () => {
-  render(
+  return(
     <Navbar inverse collapseOnSelect>
       <Navbar.Header>
         <Navbar.Brand>
@@ -12,7 +12,7 @@ const Header = () => {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav>
-          <NavItem eventKey={1} href="#">Link</NavItem>
+          <NavItem eventKey={1} href="#" className="active">Link</NavItem>
           <NavItem eventKey={2} href="#">Link</NavItem>
           <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
             <MenuItem eventKey={3.1}>Action</MenuItem>
--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { Button, PageHeader } from 'react-bootstrap';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <div className="container">
+        <PageHeader>Homie <small>First time using React</small></PageHeader>
+        <Button bsStyle="primary">alalalal</Button>
+        </div>
       </div>
     );
   }
