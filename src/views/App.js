5545105e58ecf85b1515f36189efac0187e30fdc
alik <anothermalik@gmail.com>
2017-12-23 11:06:28 +0700

    add left panel

--- src/views/App.js
+++ src/views/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
-import { Button, PageHeader } from 'react-bootstrap';
+import { Button, PageHeader, Row, Col, Panel } from 'react-bootstrap';
 
-
+const panelTitle =  (
+  <h3>Picture Here</h3>
+);
 
 class App extends Component {
   render() {
@@ -10,8 +12,20 @@ class App extends Component {
       <div className="App">
         <Header />
         <div className="container">
-        <PageHeader>Homie <small>First time using React</small></PageHeader>
-        <Button bsStyle="primary">alalalal</Button>
+          <PageHeader>Homie <small>First time using React</small></PageHeader>
+          
+            <Row>
+              <Col xs={12} md={3}>
+                <div>
+                  <Panel header={panelTitle}>
+                    Description
+                  </Panel>
+                </div>
+              </Col>
+              <Col xs={6} md={9}>
+              <Button bsStyle="primary">alalalal</Button>
+              </Col>
+            </Row>
         </div>
       </div>
     );
