0da05c39b22b5a50269ff67393663d2ad43dd23c
alik <anothermalik@gmail.com>
2018-01-06 19:32:51 +0700

    tambah table

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from '../views/App';
+import Profile from '../views/Profile';
 
 /**
 * @param exact in order for not being followed with bellow routes
@@ -10,6 +11,7 @@ const Routes = () => (
   <Router>
     <div>
       <Route exact path="/" component={App} />
+      <Route path="/profile" component={Profile} />
     </div>
   </Router>
 );
--- src/views/App.js
+++ src/views/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
-import { Button, PageHeader, Row, Col, Panel } from 'react-bootstrap';
+import { Button, PageHeader, Row, Col, Panel, Table } from 'react-bootstrap';
 
-const panelTitle =  (
+const panelTitle = (
   <h3>Picture Here</h3>
 );
 
@@ -13,19 +13,61 @@ class App extends Component {
         <Header />
         <div className="container">
           <PageHeader>Homie <small>First time using React</small></PageHeader>
-          
-            <Row>
-              <Col xs={12} md={3}>
-                <div>
-                  <Panel header={panelTitle}>
-                    Description
+
+          <Row>
+            <Col xs={12} md={3}>
+              <div>
+                <Panel header={panelTitle} bsStyle="primary">
+                  Description
                   </Panel>
-                </div>
-              </Col>
-              <Col xs={6} md={9}>
+              </div>
+            </Col>
+            <Col xs={6} md={9}>
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Table heading</th>
+                    <th>Table heading</th>
+                    <th>Table heading</th>
+                    <th>Table heading</th>
+                    <th>Table heading</th>
+                    <th>Table heading</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                  </tr>
+                </tbody>
+              </Table>
               <Button bsStyle="primary">alalalal</Button>
-              </Col>
-            </Row>
+            </Col>
+          </Row>
         </div>
       </div>
     );
